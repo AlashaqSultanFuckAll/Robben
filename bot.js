cd0020b550595d61bdccc051296437c805fcfcfe
AlashaqSultanFuckAll <40842773+AlashaqSultanFuckAll@users.noreply.github.com>
2018-10-20 22:23:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client()
 
 client.on("ready", () => {
 console.log("ready")
-client.user.setGame("five Store", "https://twitch.tv/FiveStore)
-                    }
+client.user.setGame("five Store", "https://twitch.tv/FiveStore);
+           }
   });
-client .login(BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
