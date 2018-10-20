809c7d489aba6c8829ab5f7d8061c9b85a1a9af2
AlashaqSultanFuckAll <40842773+AlashaqSultanFuckAll@users.noreply.github.com>
2018-10-20 22:21:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,6 @@ const client = new Discord.Client()
 client.on("ready", () => {
 console.log("ready")
 client.user.setGame("five Store", "https://twitch.tv/FiveStore)
-
+                    }
   });
 client .login(BOT_TOKEN);
