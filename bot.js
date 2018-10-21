879f84051aa73b9947766f46b6162c9433999947
AlashaqSultanFuckAll <40842773+AlashaqSultanFuckAll@users.noreply.github.com>
2018-10-21 08:38:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,41 +1,42 @@
-const Discord = require("discord.js")
-const client = new Discord.Client()
-const adminprefix = "$";
-const devs = ['321779633252990976'];
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const bot = new Discord.Client();
+const ms = require("ms");
+const fs = require('fs');
+const moment = require('moment');
+const request = require('request');
+const dateFormat = require('dateformat');
+const r1 = require('snekfetch');
+const jimp = require('jimp')
+const math = require('math-expression-evaluator'); 
+const child_process = require("child_process");
+const developers = ['321779633252990976'];
+const admin = "$";
+
 client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-    
-if (message.content.startsWith(adminprefix + 'setgame')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
-} else 
-  if (message.content.startsWith(adminprefix + 'setname')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
-return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+ 
+  if (message.content.startsWith(admin + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(` ☑ Client Activity Now Is : \`Watching ${argresult} \` `)
+  } else 
+  if (message.content.startsWith(admin + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(` ☑ Client Activity Now Is : \`Listening ${argresult} \` `)
+  } else 
+  if (message.content.startsWith(admin + 'setstream')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+     message.channel.send(` ☑ Client Activity Now Is : \`Streaming ${argresult} \` `)
+  }
+  if (message.content.startsWith(admin + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(` Client UserName Changed To : \` ${argresult}\` `)
 } else
-if (message.content.startsWith(adminprefix + 'setstreem')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/idk");
-    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+if (message.content.startsWith(admin + 'setavatar')) {
+  client.user.setAvatar(argresult);
+      message.channel.send(` Client Avatar Changed To : \` ${argresult}\` `)
 }
-
 });
 
-client.on('message',function(message) {
-    let args = message.content.split(" ").slice(1).join(" ");
-   if(message.content.startsWith(adminprefix + "setWatch")) {
-       if(message.author.id !== '303311682929688577') return;
-       client.user.setActivity(args,{type: 'WATCHING'});
-       message.channel.send("**- :white_check_mark: Done!,**");
-   } 
-});
-client.on('message',function(message) {
-    let args = message.content.split(" ").slice(1).join(" ");
-   if(message.content.startsWith(adminprefix + "setListen")) {
-       if(message.author.id !== '303311682929688577') return;
-       client.user.setActivity(args,{type: 'LISTENING'});
-       message.channel.send("**- :white_check_mark: Done!,**");
-   } 
-});
 client.login(BOT_TOKEN);
